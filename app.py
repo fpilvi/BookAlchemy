f62e8460d72a938dfb3f73c0457c0bbc0eb6b660
fpilvi <florentine.moehrle@gmail.com>
2025-01-08 11:13:54 +0100

    adding docstrings to app.py and data_models.py

--- app.py
+++ app.py
@@ -28,6 +28,13 @@ from datetime import datetime
 
 @app.route('/add_author', methods=['GET', 'POST'])
 def add_author():
+    """
+    Handles the route for adding a new author to the database.
+
+    If the request method is POST, the author details are collected
+    from the form and added to the database. If successful, the user is
+    redirected to the homepage.
+    """
     if request.method == 'POST':
         name = request.form['name']
         birth_date_str = request.form['birthdate']
@@ -46,6 +53,14 @@ def add_author():
 
 @app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
+    """
+    Handles the route for adding a new book to the database.
+
+    Displays a form with available authors if the request method is GET.
+    If the method is POST, the book details are collected from the form,
+    and the new book is added to the database. A success message is shown
+    after the book is added.
+    """
     authors = Author.query.all()
     if request.method == 'POST':
         title = request.form['title']
@@ -64,13 +79,22 @@ def add_book():
 
 @app.route('/')
 def home():
+    """
+    Displays the homepage with a list of all books in the database.
+    """
     books = Book.query.all()
-
     return render_template('home.html', books=books)
 
 
 @app.route('/sort_books/<sort_by>')
 def sort_books(sort_by):
+    """
+    Sorts the list of books based on the selected criteria (title or author).
+
+    The 'sort_by' parameter determines the sorting order:
+    - 'title' will sort by the book title.
+    - 'author' will sort by the author's name.
+    """
     if sort_by == 'title':
         books = Book.query.order_by(Book.title).all()
     elif sort_by == 'author':
@@ -83,6 +107,12 @@ def sort_books(sort_by):
 
 @app.route('/search_books', methods=['GET'])
 def search_books():
+    """
+    Handles the search functionality for books.
+
+    Searches for books based on the title. If a search query is provided,
+    it filters the books based on the query and displays the results.
+    """
     search_query = request.args.get('search_query')
     if search_query:
         books = Book.query.filter(Book.title.ilike(f'%{search_query}%')).all()
--- data_models.py
+++ data_models.py
@@ -2,7 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Author(db.Model):
+    """
+    Represents an author in the database.
+
+    Attributes:
+        id (int): Unique identifier for the author.
+        name (str): Name of the author.
+        birth_date (date): Birth date of the author.
+        date_of_death (date): Date of death of the author, if applicable.
+    """
     __tablename__ = 'authors'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
@@ -10,13 +20,36 @@ class Author(db.Model):
     date_of_death = db.Column(db.Date, nullable=True)
 
     def __repr__(self):
+        """
+        Returns a string representation of the Author object for debugging.
+
+        Returns:
+            str: A string in the format "<Author {name}>"
+        """
         return f"<Author {self.name}>"
 
     def __str__(self):
+        """
+        Returns a string representation of the Author object for display.
+
+        Returns:
+            str: The name of the author.
+        """
         return self.name
 
 
 class Book(db.Model):
+    """
+    Represents a book in the database.
+
+    Attributes:
+        id (int): Unique identifier for the book.
+        isbn (str): ISBN of the book (13 characters).
+        title (str): Title of the book.
+        publication_year (int): Year the book was published.
+        author_id (int): Foreign key referencing the Author table.
+        author (Author): The author of the book (relationship to Author model).
+    """
     __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     isbn = db.Column(db.String(13), unique=True, nullable=False)
@@ -28,7 +61,19 @@ class Book(db.Model):
     author = db.relationship('Author', backref='books', lazy=True)
 
     def __repr__(self):
+        """
+        Returns a string representation of the Book object for debugging.
+
+        Returns:
+            str: A string in the format "<Book {title}>"
+        """
         return f"<Book {self.title}>"
 
     def __str__(self):
+        """
+        Returns a string representation of the Book object for display.
+
+        Returns:
+            str: The title of the book.
+        """
         return self.title
